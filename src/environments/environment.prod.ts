c29391c146d2ec0d6808533339f395d4c60c5dfb
Vincent Zhao <wzhao.duplex@gmail.com>
2025-12-22 17:00:55 -0500

    update environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ec2-100-52-66-139.compute-1.amazonaws.com:8080/api'
+  apiUrl: 'http://ec2-100-52-66-139.compute-1.amazonaws.com/api'
 };
