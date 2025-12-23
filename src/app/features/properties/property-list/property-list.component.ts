a40066026a3b7900600bd8756f6880ba71b1c196
Vincent Zhao <wzhao.duplex@gmail.com>
2025-12-22 22:34:40 -0500

    hard code org id

--- src/app/features/properties/property-list/property-list.component.ts
+++ src/app/features/properties/property-list/property-list.component.ts
@@ -22,7 +22,8 @@ export class PropertyListComponent implements OnInit {
   constructor(private propertyService: PropertyService) {}
 
 ngOnInit(): void {
-  const orgId = 'b3e94ed7-e57e-49c7-9825-82ce3ef40017'; // TEMP (see step 3)
+  const orgId = '85fd0e5d-e4a6-49a6-afd6-6f14a4848783'; // TEMP (see step 3)
+  //const orgId = 'b3e94ed7-e57e-49c7-9825-82ce3ef40017'; // local
 
   this.propertyService.getAllByOrg(orgId).subscribe({
     next: (data: Property[]) => {
