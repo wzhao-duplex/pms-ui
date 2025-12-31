67279c4bfcc6788a31b7df54ff3a59d73e9150ce
Vincent Zhao <wzhao.duplex@gmail.com>
2025-12-31 14:20:01 -0500

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://ec2-100-52-66-139.compute-1.amazonaws.com/api'
-  apiUrl: 'http://pms-api-evn.us-east-2.elasticbeanstalk.com/api'
+  apiUrl: 'http://pms-api-evn.us-east-1.elasticbeanstalk.com/api'
 };
